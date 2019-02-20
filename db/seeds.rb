a3da9e11ccff968de383aa05360029360bc12b77
Changhun Yun <isaac.c.yun@gmail.com>
2019-02-20 16:59:57 +0100

    Yelp MVP done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Have a guess! I am creating restaurants! '
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'belgian',
+    phone_number: '074-2411-9988'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+    phone_number: '074-2411-9988'
+  },
+  {
+    name:         'Yauatcha',
+    address:      '15-17 Broadwick St, Soho, London W1F 0DL, UK',
+    category:     'chinese',
+    phone_number: '074-2411-9988'
+  },
+  {
+    name:         'Hakkasan Mayfair',
+    address:      '17 Bruton St, Mayfair, London W1J 6QB, UK',
+    category:     'chinese',
+    phone_number: '074-2411-9988'
+  },
+  {
+    name:         'Homeslice Neals Yard',
+    address:      '13 Neals Yard, London WC2H 9DP, UK',
+    category:     'italian',
+    phone_number: '074-2411-9988'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Best reatuarants are generated now!'
